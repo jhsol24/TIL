d4b73c7b5405a4283bf16900251ab12291a4882f
HANSOL JEONG <losnah24@gmail.com>
2021-01-13 15:12:45 +0900

    Modify

--- Algorithm_Study/0112_2884_바이러스.py
+++ Algorithm_Study/0112_2884_바이러스.py
@@ -1,26 +1,45 @@
-com = int(input())
-net = int(input())
-connects = []
-for i in range(net):
-    connects.append(list(map(int, input().split())))
-
-visited = [False]*com
-visited[0] = True
-
-def dfs(v):
-    c = visited.count(False)
-    for connect in connects:
-        if visited[connect[0]-1] is True:
-            visited[connect[1]-1] = True
-        elif visited[connect[1]-1] is True:
-            visited[connect[0]-1] = True
-    d = visited.count(False)
-    if c == d :
-        return 0
-    dfs(v)
+# 바이러스
+from collections import deque
 
 
+# BFS 메소드 정의
+def bfs(graph, start, visited):
+    count = 0
 
-dfs(1)
+    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
 
-print(visited.count(True)-1)
+    # 현재 노드를 방문 처리
+    visited[start] = True
+
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 큐에서 하나의 원소를 뽑아 출력하기
+        v = queue.popleft()
+
+        # 아직 방문하지 않은 인접한 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+                count += 1
+
+    return count
+
+
+n = int(input())
+m = int(input())
+
+graph = [[] for i in range(n + 1)]
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# 각 노드가 방문된 정보를 표현 (1차원 리스트)
+visited = [False] * (n + 1)
+
+# 정의된 BFS 함수 호출
+answer = bfs(graph, 1, visited)
+print(answer)
