4b54db16ba3c1655bbd232f7618c9585f233154e
HANSOL JEONG <losnah24@gmail.com>
2021-02-03 14:51:33 +0900

    21.02.03 | modify Implementation

--- Algorithm_Study/0203_Implementation_방_번호.py
+++ Algorithm_Study/0203_Implementation_방_번호.py
@@ -8,7 +8,7 @@ import math
 N = input()
 check = []
 # 6, 9를 제외한 숫자들의 개수를 리스트에 입력
-for i in [0,1,2,3,4,5,7,8,0]:
+for i in [0,1,2,3,4,5,7,8]:
     check.append(N.count(str(i)))
 
 # 6, 9를 뒤집을 수 있는 조건을 적용해 필요한 세트 수를 리스트에 입력
