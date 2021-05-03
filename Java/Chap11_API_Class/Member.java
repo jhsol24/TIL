cbc7cfe842db1c3f96a0846b40374276c09e3fd6
HANSOL JEONG <losnah24@gmail.com>
2021-05-04 00:09:15 +0900

    21.05.03 | add Chap11

---
+++ Java/Chap11_API_Class/Member.java
@@ -0,0 +1,58 @@
+package sec03.p460;
+
+public class Member {
+	
+	private String memberId;
+	private String memberPw;
+
+	//Getter-Setter
+	public Member(String memberId, String memberPw) {
+		super();
+		this.memberId = memberId;
+		this.memberPw = memberPw;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	
+	// Constructor
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getMemberPw() {
+		return memberPw;
+	}
+	public void setMemberPw(String memberPw) {
+		this.memberPw = memberPw;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((memberId == null) ? 0 : memberId.hashCode());
+		result = prime * result + ((memberPw == null) ? 0 : memberPw.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Member other = (Member) obj;
+		if (memberId == null) {
+			if (other.memberId != null)
+				return false;
+		} else if (!memberId.equals(other.memberId))
+			return false;
+		if (memberPw == null) {
+			if (other.memberPw != null)
+				return false;
+		} else if (!memberPw.equals(other.memberPw))
+			return false;
+		return true;
+	}
+
+}
