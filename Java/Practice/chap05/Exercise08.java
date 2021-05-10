3818fe33ee2c6e5c88fd8b1ee715c568b9d4bebc
HANSOL JEONG <losnah24@gmail.com>
2021-05-10 23:08:20 +0900

    21.05.10 | add Exercise08

---
+++ Java/Practice/chap05/Exercise08.java
@@ -0,0 +1,25 @@
+public class Exercose08 {
+    public static void main(String args[]) {
+      int[][] array = {
+          {95, 86},
+          {83, 92, 96},
+          {78, 83, 93, 87, 88}
+      };
+      
+      int sum = 0;
+      double avg = 0.0;
+      int len = 0;
+      
+      for(int i=0; i<array.length; i++) {
+        for(int j=0; j<array[i].length; j++) {
+            sum+=array[i][j];
+            len++;
+        }
+      }
+      
+      avg = (double) sum/len;
+
+      System.out.println("sum: " + sum);
+      System.out.println("avg: " + avg);
+    }
+}
