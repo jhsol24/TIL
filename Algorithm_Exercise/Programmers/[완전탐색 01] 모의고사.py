d0c47ca880aae47325a0830f020a720084988649
HANSOL JEONG <losnah24@gmail.com>
2021-06-12 00:19:38 +0900

    modify Bruce Force 01

--- Algorithm_Exercise/Programmers/[완전탐색 01] 모의고사.py
+++ Algorithm_Exercise/Programmers/[완전탐색 01] 모의고사.py
@@ -6,29 +6,33 @@ answers = [1,2,3,4,5,5,5,1,2,3,4,1,1,2,2,1,2,3,4]
 # answers = [1,2,3,4,5]
 # answers = [1,3,2,4,2]
 
-ans1 = [1,2,3,4,5]
-ans2 = [2,1,2,3,2,4,2,5]
-ans3 = [3,3,1,1,2,2,4,4,5,5]
+def solution(answers):
+    num_ans = len(answers)
 
-student1 = ans1 * (len(answers)//5) + ans1[:(len(answers)%5)]
-student2 = ans2 * (len(answers)//8) + ans2[:(len(answers)%8)]
-student3 = ans3 * (len(answers)//10) + ans3[:(len(answers)%10)]
-cnt1, cnt2, cnt3 = 0, 0, 0
-for i in range(len(answers)):
-    if answers[i] == student1[i]:
-        cnt1 += 1
-    if answers[i] == student2[i]:
-        cnt2 += 1
-    if answers[i] == student3[i]:
-        cnt3 += 1
+    student1 = [1,2,3,4,5] * (num_ans//5 + 1)
+    student2 = [2,1,2,3,2,4,2,5] * (num_ans//8 + 1)
+    student3 = [3,3,1,1,2,2,4,4,5,5] * (num_ans//10 + 1)
 
-max_ans = max(cnt1, cnt2, cnt3)
-answer = []
-if cnt1 == max_ans:
-    answer.append(1)
-if cnt2 == max_ans:
-    answer.append(2)
-if cnt3 == max_ans:
-    answer.append(3)
+    cnt1, cnt2, cnt3 = 0, 0, 0
 
-print(answer)
+    for i in range(len(answers)):
+        if answers[i] == student1[i]:
+            cnt1 += 1
+        if answers[i] == student2[i]:
+            cnt2 += 1
+        if answers[i] == student3[i]:
+            cnt3 += 1
+
+    max_ans = max(cnt1, cnt2, cnt3)
+    answer = []
+
+    if cnt1 == max_ans:
+        answer.append(1)
+    if cnt2 == max_ans:
+        answer.append(2)
+    if cnt3 == max_ans:
+        answer.append(3)
+
+    return answer
+
+print(solution(answers))
