6a49f475d950848de65674dfc01fd381c942d84c
HANSOL JEONG <losnah24@gmail.com>
2021-07-21 23:16:11 +0900

    modify BOJ heap

--- Algorithm_Exercise/BOJ/0707_힙_가운데를말해요.py
+++ Algorithm_Exercise/BOJ/0707_힙_가운데를말해요.py
@@ -2,20 +2,47 @@
 # 가운데를 말해요
 # https://www.acmicpc.net/problem/1655
 
-N = int(input())
-arr = []
-for i in range(N):
-    arr.append(int(input()))
+# 시간초과로 실패
 
-answer = []
-for j in range(1, N+1):
-    if j % 2 != 0:
-        answer.append(sorted(arr[:j])[j//2])
+# N = int(input())
+# arr = []
+# for i in range(N):
+#     arr.append(int(input()))
+#
+# answer = []
+# for j in range(1, N+1):
+#     if j % 2 != 0:
+#         answer.append(sorted(arr[:j])[j//2])
+#     else:
+#         answer.append(sorted(arr[:j])[j//2 - 1])
+#
+# for k in range(N):
+#     print(answer[k])
+
+
+# 힙 자료구조를 써서 다시 풀이
+
+import heapq
+import sys
+
+n = int(sys.stdin.readline())
+
+max_heap = []
+min_heap = []
+
+for _ in range(n):
+    num = int(sys.stdin.readline())
+
+    if len(min_heap) == len(max_heap):
+        heapq.heappush(min_heap, -num)
     else:
-        answer.append(sorted(arr[:j])[j//2 - 1])
+        heapq.heappush(max_heap, num)
 
-for k in range(N):
-    print(answer[k])
+    if max_heap and (-min_heap[0] > max_heap[0]):
+        heapq.heappush(min_heap, -heapq.heappop(max_heap))
+        heapq.heappush(max_heap, -heapq.heappop(min_heap))
+
+    print(-min_heap[0])
 
 
-# 다시 풀기
+
